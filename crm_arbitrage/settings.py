9214f8be4f5efe3558689632af244deb821fc14c
Oleksandr Kolomoiets <0985305543@ukr.net>
2023-06-05 18:23:23 +0300

    Added customer_management app

--- crm_arbitrage/settings.py
+++ crm_arbitrage/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "customer_management",
     "user_management",
     "campaign_management",
     "partner_management",
