6263fb923382c16a4c4b5065163bff4ed0d20dce
Oleksandr Kolomoiets <0985305543@ukr.net>
2023-06-10 09:10:40 +0300

    Adjusted model of the api_teams

--- api_teams/models.py
+++ api_teams/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from api_users.models import CustomUser, Position
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    team_lead = models.OneToOneField(
+        CustomUser,
+        on_delete=models.SET_NULL,
+        null=True,
+        limit_choices_to={'position': Position.TEAM_LEAD},
+        related_name="teams_lead",
+    )
+    members = models.ManyToManyField(
+        CustomUser,
+        limit_choices_to={'position': Position.BUYER},
+        related_name="teams_member",
+        blank=True,
+    )
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
