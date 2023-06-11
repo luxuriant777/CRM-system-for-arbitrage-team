db1e271c10def56af87237d423b73b2f8f0a16ba
Oleksandr Kolomoiets <0985305543@ukr.net>
2023-06-11 13:57:25 +0300

    Added creation of groups and modified creation of permissions

---
+++ api_users/management/commands/create_groups.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from api_users.models import Position
+
+
+class Command(BaseCommand):
+    help = "Create groups and assign permissions based on user roles"
+
+    def handle(self, *args, **options):
+        buyer_permissions = [
+            "view_lead", 
+            "view_order", 
+            "view_team"
+        ]
+        
+        team_lead_permissions = [
+            "view_lead", 
+            "add_lead", 
+            "change_lead", 
+            "delete_lead", 
+            "view_order", 
+            "add_order", 
+            "change_order", 
+            "delete_order", 
+            "view_team", 
+            "add_team", 
+            "change_team", 
+            "delete_team"
+        ]
+
+        owner_permissions = Permission.objects.all().values_list("codename", flat=True)
+
+        groups = {
+            Position.BUYER: buyer_permissions,
+            Position.TEAM_LEAD: team_lead_permissions,
+            Position.OWNER: owner_permissions
+        }
+
+        for group_name, permission_codenames in groups.items():
+            group, created = Group.objects.get_or_create(name=group_name)
+
+            for codename in permission_codenames:
+                permission = Permission.objects.get(codename=codename)
+                if not group.permissions.filter(id=permission.id).exists():
+                    group.permissions.add(permission)
+
+        self.stdout.write(self.style.SUCCESS("Groups are created successfully."))
--- core/management/commands/check.py
+++ core/management/commands/check.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core import management
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
+from api_users.models import Position
 
 
 class Command(BaseCommand):
@@ -10,9 +11,20 @@ class Command(BaseCommand):
         necessary_permissions = ["view_team_lead", "add_team_lead", "change_team_lead", "delete_team_lead",
                                  "view_buyer", "add_buyer", "change_buyer", "delete_buyer"]
 
-        for perm_codename in necessary_permissions:
-            if not Permission.objects.filter(codename=perm_codename).exists():
-                # If not, call the "create_permissions" command.
-                self.stdout.write(self.style.WARNING("Some necessary permissions are missing. Creating them now..."))
-                management.call_command("create_permissions")
-                break  # After calling "create_permissions", there is no need to continue checking permissions.
+        necessary_groups = [Position.BUYER, Position.TEAM_LEAD, Position.OWNER]
+
+        permissions_exist = all(Permission.objects.filter(codename=perm_codename).exists()
+                                for perm_codename in necessary_permissions)
+
+        groups_exist = all(Group.objects.filter(name=group_name).exists()
+                           for group_name in necessary_groups)
+
+        if not permissions_exist:
+            # If necessary permissions do not exist, call the "create_permissions" command.
+            self.stdout.write(self.style.WARNING("Some necessary permissions are missing. Creating them now..."))
+            management.call_command("create_permissions")
+
+        if not groups_exist:
+            # If necessary groups do not exist, call the "create_groups" command.
+            self.stdout.write(self.style.WARNING("Some necessary groups are missing. Creating them now..."))
+            management.call_command("create_groups")
