4db4fc13e824d9fa5b3edbcd07a3f75bdd7063f8
Oleksandr Kolomoiets <0985305543@ukr.net>
2023-06-11 16:31:18 +0300

    Fixed PUT method for teams

--- api_teams/views.py
+++ api_teams/views.py
@@ -2,12 +2,29 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Team
 from .serializers import TeamSerializer
+from rest_framework import permissions
+
+
+class TeamPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # "GET", "HEAD", or "OPTIONS"
+            return request.user.has_perm("api_teams.view_team")
+
+        # Map method to permission codename
+        method_permissions = {
+            "POST": "api_teams.add_team",
+            "PUT": "api_teams.change_team",
+            "PATCH": "api_teams.change_team",
+            "DELETE": "api_teams.delete_team",
+        }
+
+        return request.user.has_perm(method_permissions.get(request.method, ""))
 
 
 class TeamCreateView(generics.CreateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TeamPermission]
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
@@ -16,22 +33,22 @@ class TeamCreateView(generics.CreateAPIView):
 class TeamListView(generics.ListAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TeamPermission]
 
 
 class TeamDetailView(generics.RetrieveAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TeamPermission]
 
 
 class TeamUpdateView(generics.UpdateAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TeamPermission]
 
 
 class TeamDeleteView(generics.DestroyAPIView):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, TeamPermission]
